8acfc7b60c1d5482582cd1fc262de41906b5904b
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-09-02 01:10:44 +0530

    add update post functionality

--- client/src/Components/DashPosts.jsx
+++ client/src/Components/DashPosts.jsx
@@ -1,27 +1,27 @@
-import { Modal, Table,Button } from 'flowbite-react';
+import { Modal, Table, Button } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { set } from 'mongoose';
 
 export default function DashPosts() {
-  const { currentUser } = useSelector ((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
   const [showMore, setShowMore] = useState(true);
   const [showModal, setShowModal] = useState(false);
   const [postIdToDelete, setPostIdToDelete] = useState('');
-  
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
         const data = await res.json();
-        if(res.ok) {
+        if (res.ok) {
           setUserPosts(data.posts);
-          if(data.posts.length < 9) {
+          if (data.posts.length < 9) {
             setShowMore(false);
           }
-        }        
+        }
       } catch (error) {
         console.log(error.message);
       }
@@ -34,7 +34,9 @@ export default function DashPosts() {
   const handleShowMore = async () => {
     const startIndex = userPosts.length;
     try {
-      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
       const data = await res.json();
       if (res.ok) {
         setUserPosts((prev) => [...prev, ...data.posts]);
@@ -43,14 +45,15 @@ export default function DashPosts() {
         }
       }
     } catch (error) {
-      console.log(error.message);      
+      console.log(error.message);
     }
   };
 
   const handleDeletePost = async () => {
     setShowModal(false);
     try {
-      const res = await fetch (`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,
+      const res = await fetch(
+        `/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,
         {
           method: 'DELETE',
         }
@@ -59,15 +62,15 @@ export default function DashPosts() {
       if (!res.ok) {
         console.log(data.message);
       } else {
-        setUserPosts((prev) => 
+        setUserPosts((prev) =>
           prev.filter((post) => post._id !== postIdToDelete)
         );
       }
     } catch (error) {
       console.log(error.message);
-      
     }
-  }
+  };
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -79,34 +82,50 @@ export default function DashPosts() {
               <Table.HeadCell>Post title</Table.HeadCell>
               <Table.HeadCell>Category</Table.HeadCell>
               <Table.HeadCell>Delete</Table.HeadCell>
-              <Table.HeadCell><span>Edit</span></Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
             </Table.Head>
-            {userPosts.map(() => (
+            {userPosts.map((post) => (
               <Table.Body className='divide-y'>
                 <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
-                  <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
                   <Table.Cell>
                     <Link to={`/post/${post.slug}`}>
-                      <img 
+                      <img
                         src={post.image}
                         alt={post.title}
                         className='w-20 h-10 object-cover bg-gray-500'
                       />
                     </Link>
                   </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className='font-medium text-gray-900 dark:text-white'
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
-                    <span className='font-medium text-red-500 hover:underline cursor-pointer'
-                    onClick={() => {
-                      setShowModal(true);
-                      setPostIdToDelete(post._id);
-                    }}
+                    <span
+                      onClick={() => {
+                        setShowModal(true);
+                        setPostIdToDelete(post._id);
+                      }}
+                      className='font-medium text-red-500 hover:underline cursor-pointer'
                     >
                       Delete
                     </span>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
+                    <Link
+                      className='text-teal-500 hover:underline'
+                      to={`/update-post/${post._id}`}
+                    >
                       <span>Edit</span>
                     </Link>
                   </Table.Cell>
@@ -114,13 +133,14 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
-          {
-            showMore && (
-              <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
-                Show more
-              </button>
-            )
-          }
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
+          )}
         </>
       ) : (
         <p>You have no posts yet!</p>
@@ -150,5 +170,5 @@ export default function DashPosts() {
         </Modal.Body>
       </Modal>
     </div>
-  )
+  );
 }
