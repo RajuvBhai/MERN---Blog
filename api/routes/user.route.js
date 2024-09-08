c409752b2218b636c29ffc499862bc634582c0f8
RajuvBhai <rajuvkaranrajan1212@gmail.com>
2024-09-08 17:26:47 +0530

    user page api correction

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,8 +5,8 @@ import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.get('/test', test);
-router.put('/update/:userId', verifyToken,updateUser);
-router.delete('/delete/:userId', verifyToken,deleteUser);
+router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getUsers);
 router.get('/:userId', getUser);
